d36eac47c81d5f54c3c4cd913cc446894ec78bdc
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-07-30 13:58:46 -0400

    Enable compressors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Gemcutter::Application.configure do
   config.active_support.deprecation = :notify
   config.serve_static_assets = $rubygems_config[:asset_cacher]
   config.i18n.fallbacks = true
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :scss
 end
 
 require Rails.root.join("config", "secret") if Rails.root.join("config", "secret.rb").file?
